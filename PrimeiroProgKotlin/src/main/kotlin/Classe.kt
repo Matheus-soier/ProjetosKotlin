9675f3e3d484d798a0349befe2d02287b2d8b070
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-10-22 02:54:31 -0300

    Conteúdo iniciado sobre Classe e Classe generica em kotlin

--- PrimeiroProgKotlin/src/main/kotlin/Classe.kt
+++ PrimeiroProgKotlin/src/main/kotlin/Classe.kt
@@ -1,3 +1,12 @@
+class Cliente(val nome: String, val idade: Int)
+class Contato(val id: Int, var email: String?)
+
 fun main() {
-    
+
+    val Cliente = Cliente("Matheus Sampaio Soier", 20)
+    val Contato = Contato(111, "business.soier@gmail.com")
+
+    Contato.email = "devmatheus.soier@gmail.com"
+
+    println("Nome do cliente: ${Cliente.nome} \nIdade do cliente: ${Cliente.idade} \nId do Cliente: ${Contato.id} \nEmail do Cliente: ${Contato.email}")
 }
