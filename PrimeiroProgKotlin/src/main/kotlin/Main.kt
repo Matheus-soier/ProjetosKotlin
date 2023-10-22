212f910aa3aba707a84b77c2f0ece218a861b919
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-10-22 01:17:01 -0300

    Finalizando conteúdo de funções

--- PrimeiroProgKotlin/src/main/kotlin/Main.kt
+++ PrimeiroProgKotlin/src/main/kotlin/Main.kt
@@ -23,4 +23,16 @@ fun main() { //Ponto de entrada de um código Kotlin, definido pela função [ma
     }
 
     executar()
+
+    //Vararg -> Permite quantidade de parâmetros indefinidos dentro de uma mesma variável, jogando os dados em um array
+    fun textos(vararg text: String) {
+        for (t in text) println(t)
+    }
+    textos("Português", "Ingles", "Espanhol", "Alemão", "Francês", "Russo", "Mandarim")
+
+    //Executando função com parâmetros nomeados juntamente com array de String em uma sobrecarga de método e parâmetros
+    fun textos(vararg text: String, prefixo: String) {
+        for (t in text) println("$prefixo: $t")
+    }
+    textos( "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", prefixo = "Número")
 }
